0eb8b3770d654bba947bd290612f5858d0b24a3a
MissMuffinman <32392933+MissMuffinman@users.noreply.github.com>
2021-02-04 21:16:27 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const config = require('./config.json')
+const mongo = require('./mongo')
+const commands = require('./commands')
+
+const prefix = (config.prefix)
+
+client.once('ready', async () => {
+    console.log('LogBook is online!')
+
+    commands(client) 
+    
+    let activities = [`BE`, `BTS`, `BA memes`   ],i = 0;
+    setInterval(() => client.user.setActivity(`${activities[i++ %  activities.length]}`,  
+                    {type:"STREAMING",url:"https://www.youtube.com/watch?v=-5q5mZbe3V8"  }), 10000)       
+});
+
+client.login(config.token)
