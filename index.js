aa5df97c2308d47de4140bd9654401bdde3f92c4
MissMuffinman <32392933+MissMuffinman@users.noreply.github.com>
2021-02-15 19:43:00 -0600

    Updating Sejin

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
-const fs = require('fs');
-const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
+const config = require('./config.json')
+const mongo = require('./mongo')
+const commands = require('./commands')
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const prefix = (config.prefix)
 
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
-}
+client.once('ready', async () => {
+    console.log('LogBook is online!')
 
-client.once('ready', () => {
-    console.log('Manager Sejin is online!');
-
-    const activities = ['BE', 'BTS', 'BA memes' ];
-    let i = 0;
-    setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`,
-        { type:'STREAMING', url:'https://www.youtube.com/watch?v=-5q5mZbe3V8' }), 10000);
+    commands(client) 
+    
+    let activities = [`BE`, `BTS`, `BA memes`],i = 0;
+    setInterval(() => client.user.setActivity(`${activities[i++ %  activities.length]}`,  
+                    {type:"STREAMING",url:"https://www.youtube.com/watch?v=-5q5mZbe3V8"  }), 10000)       
 });
 
-client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift();
-    if (!client.commands.has(command)) return;
-
-    try {
-        client.commands.get(command).execute(message, args);
-    }
-    catch (error) {
-        console.error(error);
-        message.reply('There was an error trying to execute that command!' + command.name);
-    }
-});
-
-client.login(token);
+client.login(config.token)
