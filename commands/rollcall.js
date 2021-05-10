c1c927e52eff5a539761476c1d1ce2ed86e535cb
Mariana <marianagaldomartinez@gmail.com>
2021-05-10 01:23:44 -0300

    addded commends to add active role, rollcall role and to do roll call

---
+++ commands/rollcall.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+var path = require("path");
+const fileName = '../config.json';
+var pathToJson = path.resolve(__dirname, fileName);
+const file = require(pathToJson);
+
+const activeRoleID = file.activeRole;
+const rollCallID = file.rollcallRole;
+
+module.exports = {
+    commands: 'rollcall',
+    callback:  async (message) => {
+
+
+        if (message.author.bot) return
+        const { content, channel } = message
+        let text = content;
+
+        var members = message.guild.members.cache;
+        var rollcalled = 0;
+        var activeMembers = 0;
+
+        for (const [member, memberInfo] of members.entries()) {
+            if(memberInfo.user.bot){
+                continue;
+            }
+
+
+            if(!memberInfo.roles.cache.get(activeRoleID)){
+                console.log([rollCallID]);
+                memberInfo.roles.add([rollCallID]);
+                rollcalled++;
+                //members.get(member).roles.add([rollCallID]);
+            }
+            else{
+                activeMembers++;
+            }
+        }
+        return channel.send(`Rollcall done. ${rollcalled} are in roll call. ${activeMembers} active members.`);
+    }
+}
